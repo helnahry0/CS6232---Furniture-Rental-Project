1be089d6f6a2cdb990e970b657d6318e1d5468f3
bwarren3 <bwarren3@my.westga.edu>
2026-03-26 23:15:00 -0400

    Created Employee Model

---
+++ FurnitureRental/Model/Employee.cs
@@ -0,0 +1,19 @@
+﻿namespace FurnitureRental.Model
+{
+    public class Employee
+    {
+        public int EmployeeId { get; set; }
+
+        public string Role { get; set; } = string.Empty;
+
+        public string FirstName { get; set; } = string.Empty;
+
+        public string LastName { get; set; } = string.Empty;
+
+        public string Username { get; set; } = string.Empty;
+
+        public string Password { get; set; } = string.Empty;
+
+        public string FullName => $"{FirstName} {LastName}".Trim();
+    }
+}
