a886c09da3d9a547f7df574a723c3929073ecd14
bwarren3 <bwarren3@my.westga.edu>
2026-03-26 23:16:36 -0400

    Controller class to show current user

---
+++ FurnitureRental/Controller/Currentuser.cs
@@ -0,0 +1,21 @@
+﻿using FurnitureRental.Model;
+
+namespace FurnitureRental
+{
+    public static class CurrentUser
+    {
+        public static Employee? Employee { get; private set; }
+
+        public static bool IsLoggedIn => Employee != null;
+
+        public static void Login(Employee employee)
+        {
+            Employee = employee;
+        }
+
+        public static void Logout()
+        {
+            Employee = null;
+        }
+    }
+}
