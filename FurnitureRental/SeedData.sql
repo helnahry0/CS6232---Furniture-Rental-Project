3996409e280fc29b049bdce27150e9f92f7016a3
bwarren3 <bwarren3@my.westga.edu>
2026-03-26 23:14:48 -0400

    Added Employee seed data

--- FurnitureRental/SeedData.sql
+++ FurnitureRental/SeedData.sql
@@ -10,3 +10,12 @@ VALUES
 ('Cathy', 'Brown',   'Female', '2001-02-14', '6785554444', '321 Cedar Rd', NULL, 'Decatur', 'GA', '30030'),
 ('David', 'Miller',  'Male',   '1987-07-09', '4705555555', '654 Birch Ln', NULL, 'Roswell', 'GA', '30075');
 GO
+
+/* Employee Seeds */
+INSERT INTO dbo.Employee
+(role, dob, first_name, last_name, phone, address1, address2, city, state, zip, username, password)
+VALUES
+('Admin', '1995-08-20', 'Jane', 'Smith', '4785552222', '456 Oak Rd', NULL, 'Warner Robins', 'GA', '31088', 'admin1', 'admin123'),
+('Employee', '2000-01-01', 'Brendan', 'Warren', '4785551111', '123 Main St', NULL, 'Macon', 'GA', '31210', 'bwarren', 'test123'),
+('Employee', '1999-03-10', 'Alex', 'Taylor', '7705553333', '789 Pine Ave', NULL, 'Atlanta', 'GA', '30301', 'ataylor', 'test456');
+GO
