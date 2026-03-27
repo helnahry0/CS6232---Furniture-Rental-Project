4962e777968a088ecfbeeb523178383f50ba923f
bwarren3 <bwarren3@my.westga.edu>
2026-03-26 23:16:10 -0400

    Employee DB created, along with creating a DBHelper for the connection string

---
+++ FurnitureRental/DBAcess/DbHelper.cs
@@ -0,0 +1,10 @@
+﻿using System.Configuration;
+
+namespace FurnitureRental.DBAcess
+{
+    public static class DbHelper
+    {
+        public static string ConnectionString =>
+            ConfigurationManager.ConnectionStrings["FurnitureRentalDb"].ConnectionString;
+    }
+}
---
+++ FurnitureRental/DBAcess/EmployeeDbDal.cs
@@ -0,0 +1,50 @@
+﻿using FurnitureRental.Model;
+using Microsoft.Data.SqlClient;
+using System;
+using System.Data;
+
+namespace FurnitureRental.DBAcess
+{
+    public class EmployeeDbDal
+    {
+        public Employee? AuthenticateEmployee(string username, string password)
+        {
+            const string query = @"
+                SELECT
+                    employee_id,
+                    role,
+                    first_name,
+                    last_name,
+                    username,
+                    password
+                FROM dbo.Employee
+                WHERE username = @Username
+                  AND password = @Password;";
+
+            using SqlConnection connection = new SqlConnection(DbHelper.ConnectionString);
+            using SqlCommand command = new SqlCommand(query, connection);
+
+            command.Parameters.Add("@Username", SqlDbType.VarChar).Value = username;
+            command.Parameters.Add("@Password", SqlDbType.VarChar).Value = password;
+
+            connection.Open();
+
+            using SqlDataReader reader = command.ExecuteReader();
+
+            if (!reader.Read())
+            {
+                return null;
+            }
+
+            return new Employee
+            {
+                EmployeeId = Convert.ToInt32(reader["employee_id"]),
+                Role = Convert.ToString(reader["role"]) ?? string.Empty,
+                FirstName = Convert.ToString(reader["first_name"]) ?? string.Empty,
+                LastName = Convert.ToString(reader["last_name"]) ?? string.Empty,
+                Username = Convert.ToString(reader["username"]) ?? string.Empty,
+                Password = Convert.ToString(reader["password"]) ?? string.Empty
+            };
+        }
+    }
+}
