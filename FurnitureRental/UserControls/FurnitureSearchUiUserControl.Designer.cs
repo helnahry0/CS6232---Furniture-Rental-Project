c6b8cd8e3cb97cc6a65453839e4d64b06ff8ab50
Masterwale <Oopanug1@my.westga.edu>
2026-04-10 17:54:27 -0400

    Furniture search ui commit

--- FurnitureRental/UserControls/FurnitureSearchUiUserControl.Designer.cs
+++ FurnitureRental/UserControls/FurnitureSearchUiUserControl.Designer.cs
@@ -28,17 +28,146 @@
         /// </summary>
         private void InitializeComponent()
         {
+            FurnitureSearchLabel = new Label();
+            CategoryLabel = new Label();
+            StyleLabel = new Label();
+            FurnitureIdLabel = new Label();
+            FurnitureNameLabel = new Label();
+            CategoryComboBox = new ComboBox();
+            StyleComboBox = new ComboBox();
+            FurnitureIdTextBox = new TextBox();
+            FurnitureNameTextBox = new TextBox();
+            SearchButton = new Button();
+            AddToCartButton = new Button();
             SuspendLayout();
+            // 
+            // FurnitureSearchLabel
+            // 
+            FurnitureSearchLabel.AutoSize = true;
+            FurnitureSearchLabel.Location = new Point(104, 81);
+            FurnitureSearchLabel.Name = "FurnitureSearchLabel";
+            FurnitureSearchLabel.Size = new Size(115, 20);
+            FurnitureSearchLabel.TabIndex = 0;
+            FurnitureSearchLabel.Text = "Furniture Search";
+            // 
+            // CategoryLabel
+            // 
+            CategoryLabel.AutoSize = true;
+            CategoryLabel.Location = new Point(103, 119);
+            CategoryLabel.Name = "CategoryLabel";
+            CategoryLabel.Size = new Size(84, 20);
+            CategoryLabel.TabIndex = 1;
+            CategoryLabel.Text = "Category   :";
+            // 
+            // StyleLabel
+            // 
+            StyleLabel.AutoSize = true;
+            StyleLabel.Location = new Point(103, 165);
+            StyleLabel.Name = "StyleLabel";
+            StyleLabel.Size = new Size(84, 20);
+            StyleLabel.TabIndex = 2;
+            StyleLabel.Text = "Style          :";
+            // 
+            // FurnitureIdLabel
+            // 
+            FurnitureIdLabel.AutoSize = true;
+            FurnitureIdLabel.Location = new Point(98, 211);
+            FurnitureIdLabel.Name = "FurnitureIdLabel";
+            FurnitureIdLabel.Size = new Size(89, 20);
+            FurnitureIdLabel.TabIndex = 3;
+            FurnitureIdLabel.Text = "Furniture ID:";
+            // 
+            // FurnitureNameLabel
+            // 
+            FurnitureNameLabel.AutoSize = true;
+            FurnitureNameLabel.Location = new Point(98, 286);
+            FurnitureNameLabel.Name = "FurnitureNameLabel";
+            FurnitureNameLabel.Size = new Size(114, 20);
+            FurnitureNameLabel.TabIndex = 4;
+            FurnitureNameLabel.Text = "Furniture Name:";
+            // 
+            // CategoryComboBox
+            // 
+            CategoryComboBox.FormattingEnabled = true;
+            CategoryComboBox.Location = new Point(193, 119);
+            CategoryComboBox.Name = "CategoryComboBox";
+            CategoryComboBox.Size = new Size(215, 28);
+            CategoryComboBox.TabIndex = 5;
+            // 
+            // StyleComboBox
+            // 
+            StyleComboBox.FormattingEnabled = true;
+            StyleComboBox.Location = new Point(193, 165);
+            StyleComboBox.Name = "StyleComboBox";
+            StyleComboBox.Size = new Size(215, 28);
+            StyleComboBox.TabIndex = 6;
+            // 
+            // FurnitureIdTextBox
+            // 
+            FurnitureIdTextBox.Location = new Point(193, 211);
+            FurnitureIdTextBox.Name = "FurnitureIdTextBox";
+            FurnitureIdTextBox.Size = new Size(215, 27);
+            FurnitureIdTextBox.TabIndex = 7;
+            // 
+            // FurnitureNameTextBox
+            // 
+            FurnitureNameTextBox.Location = new Point(218, 283);
+            FurnitureNameTextBox.Name = "FurnitureNameTextBox";
+            FurnitureNameTextBox.Size = new Size(190, 27);
+            FurnitureNameTextBox.TabIndex = 8;
+            // 
+            // SearchButton
+            // 
+            SearchButton.Location = new Point(93, 372);
+            SearchButton.Name = "SearchButton";
+            SearchButton.Size = new Size(94, 29);
+            SearchButton.TabIndex = 9;
+            SearchButton.Text = "Search";
+            SearchButton.UseVisualStyleBackColor = true;
+            // 
+            // AddToCartButton
+            // 
+            AddToCartButton.Location = new Point(231, 372);
+            AddToCartButton.Name = "AddToCartButton";
+            AddToCartButton.Size = new Size(148, 29);
+            AddToCartButton.TabIndex = 10;
+            AddToCartButton.Text = "Add To Cart";
+            AddToCartButton.UseVisualStyleBackColor = true;
             // 
             // FurnitureSearchUiUserControl
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
+            Controls.Add(AddToCartButton);
+            Controls.Add(SearchButton);
+            Controls.Add(FurnitureNameTextBox);
+            Controls.Add(FurnitureIdTextBox);
+            Controls.Add(StyleComboBox);
+            Controls.Add(CategoryComboBox);
+            Controls.Add(FurnitureNameLabel);
+            Controls.Add(FurnitureIdLabel);
+            Controls.Add(StyleLabel);
+            Controls.Add(CategoryLabel);
+            Controls.Add(FurnitureSearchLabel);
+            Margin = new Padding(3, 4, 3, 4);
             Name = "FurnitureSearchUiUserControl";
-            Size = new Size(637, 415);
+            Size = new Size(1332, 531);
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label FurnitureSearchLabel;
+        private Label CategoryLabel;
+        private Label StyleLabel;
+        private Label FurnitureIdLabel;
+        private Label FurnitureNameLabel;
+        private ComboBox CategoryComboBox;
+        private ComboBox StyleComboBox;
+        private TextBox FurnitureIdTextBox;
+        private TextBox FurnitureNameTextBox;
+        private Button SearchButton;
+        private Button AddToCartButton;
     }
 }
--- FurnitureRental/UserControls/FurnitureSearchUiUserControl.cs
+++ FurnitureRental/UserControls/FurnitureSearchUiUserControl.cs
@@ -16,5 +16,6 @@ namespace FurnitureRental.UserControls
         {
             InitializeComponent();
         }
+
     }
 }
